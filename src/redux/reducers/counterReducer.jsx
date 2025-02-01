a51190cd5b174d993ddae334204f4b524241fa5e
ozkan <ozkandnz09@gmail.com>
2025-02-02 00:12:28 +0300

    =

--- src/redux/reducers/counterReducer.jsx
+++ src/redux/reducers/counterReducer.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const counterReducer = (state:{sayac:0,text:""}, action1) => {
+const counterReducer = (state={sayac:0,text:""}, action1) => {
   return (
     <div>
       
