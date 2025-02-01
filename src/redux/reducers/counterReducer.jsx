6ef7552c63ead27b405bc07b8e1ba1f066a6150d
ozkan <ozkandnz09@gmail.com>
2025-02-02 00:39:59 +0300

    arttır

--- src/redux/reducers/counterReducer.jsx
+++ src/redux/reducers/counterReducer.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
+import React from "react";
 
 // const counterReducer = (state={sayac:0,text:""}, action1) => {
-const counterReducer = (state={sayac:0,text:""}, {type,payload1}) => {
- 
-
-switch (type) {
+const counterReducer = (state = { sayac: 0, text: "" }, { type, payload1 }) => {
+  switch (type) {
     case "ARTTIR":
-        
-       
+      return { sayac: state.sayac + 1, text: payload1 };
 
     default:
-    
-
-}
-
-export default counterReducer
+      return state;
+  }
+};
+export default counterReducer;
