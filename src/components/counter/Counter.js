ac110d736b30e36afc6634f3439a58b12e8ed49d
ozkan <ozkandnz09@gmail.com>
2025-02-01 23:24:16 +0300

    counter

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import "./Counter.css"
 
 const Counter = () => {
   return (
-    <div>
-      Counter
+    <div className='app'>
+      <h2 className='counter-header'>Counter With Redux</h2>
+
+      <h1></h1>
+      {/*sayac*/}
+
+      <h2></h2>
+      {/*text*/}
+
+      <div>
+        <button className='counter-button positive'>ARTTIR</button>
+        <button className='counter-button '>RESET</button>
+        <button className='counter-button negative'>AZALT</button>
+      </div>
     </div>
   )
 }
