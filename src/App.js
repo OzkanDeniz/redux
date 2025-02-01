9bb169550d46d7eafd380dc0e0bab7fdafbe6250
ozkan <ozkandnz09@gmail.com>
2025-02-01 23:11:19 +0300

    provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from "react";
-
 import "./App.css";
+import Counter from "./components/counter/Counter";
+import { Provider } from "react-redux";
 
 const App = () => {
-  return <div>app</div>;
+  return (
+    <div className="app">
+      <Provider>
+        <Counter />
+      </Provider>
+    </div>
+  );
 };
 
 export default App;
