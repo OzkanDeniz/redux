bb3a63e425f2340a3aedc0949c830d07654e8986
ozkan <ozkandnz09@gmail.com>
2025-02-02 00:31:12 +0300

    action=>type,payload

--- src/redux/reducers/counterReducer.jsx
+++ src/redux/reducers/counterReducer.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
-const counterReducer = (state={sayac:0,text:""}, action1) => {
+// const counterReducer = (state={sayac:0,text:""}, action1) => {
+const counterReducer = (state={sayac:0,text:""}, {type,payload1}) => {
   return (
     <div>
       
