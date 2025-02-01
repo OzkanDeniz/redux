3409df932cf5ad6bc772b03866dafaec3c4895f0
ozkan <ozkandnz09@gmail.com>
2025-02-02 00:47:50 +0300

    action

--- src/redux/actions/counterActions.jsx
+++ src/redux/actions/counterActions.jsx
@@ -1,11 +1,14 @@
 //arttır,azalt,sıfırla
 
-
 //!garson
-export const arttir =()=>({
-
-type:"ARTTIR", payload1:"ARTTIRILDI"
-
-
-
-})
+export const arttir = () => ({
+  type: "ARTTIR",
+  payload1: "ARTTIRILDI",
+});
+export const sifirla = () => ({
+  type: "RESET",
+  payload1: "SIFIRLANDI",
+});
+export const azalt = () => ({
+  type: "AZALT",
+});
