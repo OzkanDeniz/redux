ad804151d0dcee364f5b55a2a43b20bb58c7e220
ozkan <ozkandnz09@gmail.com>
2025-02-02 00:34:08 +0300

    switch

--- src/redux/reducers/counterReducer.jsx
+++ src/redux/reducers/counterReducer.jsx
@@ -2,11 +2,16 @@ import React from 'react'
 
 // const counterReducer = (state={sayac:0,text:""}, action1) => {
 const counterReducer = (state={sayac:0,text:""}, {type,payload1}) => {
-  return (
-    <div>
-      
-    </div>
-  )
+ 
+
+switch (type) {
+    case "ARTTIR":
+        
+       
+
+    default:
+    
+
 }
 
 export default counterReducer
