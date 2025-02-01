a16bce61771a22136484353ade6a2e293c425ccc
ozkan <ozkandnz09@gmail.com>
2025-02-02 00:23:25 +0300

    useDispatch

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,34 +1,36 @@
-import React from 'react'
-import "./Counter.css"
-import { useSelector } from 'react-redux'
-import counterReducer from '../../redux/reducers/counterReducer'
+import React from "react";
+import "./Counter.css";
+import { useDispatch, useSelector } from "react-redux";
+import counterReducer from "../../redux/reducers/counterReducer";
 
 const Counter = () => {
   //değişkenler useSelector hookuyla çağırılır, fonksiyonlar ise useDispatch ile çağırılır.
-  const {sayac,text} = useSelector((state)=>state.counterReducer)
+  const { sayac, text } = useSelector((state) => state.counterReducer);
 
-
+  const dispatchhh = useDispatch();
 
   return (
-    <div className='app'>
-      <h2 className='counter-header'>Counter With Redux</h2>
+    <div className="app">
+      <h2 className="counter-header">Counter With Redux</h2>
 
-      <h1></h1>
+      <h1>{sayac}</h1>
       {/*sayac*/}
 
-      <h2></h2>
+      <h2>{text}</h2>
       {/*text*/}
 
       <div>
-        <button className='counter-button positive'
-
-        
-        >ARTTIR</button>
-        <button className='counter-button '>RESET</button>
-        <button className='counter-button negative'>AZALT</button>
+        <button
+          className="counter-button positive"
+          onClick={() => dispatchhh(arttir())}
+        >
+          ARTTIR
+        </button>
+        <button className="counter-button ">RESET</button>
+        <button className="counter-button negative">AZALT</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Counter
+export default Counter;
