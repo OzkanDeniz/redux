e64ae0da8dad9c478ca9048192dc86ea240ef018
ozkan <ozkandnz09@gmail.com>
2025-02-02 21:55:16 +0300

    e

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -2,9 +2,17 @@ import React, { useState } from "react";
 
 const TodoInput = () => {
   const [todo, setTodo] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <form>
-      <input className="todo-input" type="text" placeholder="Add Todo" onChange={(e)=>setTodo(e.target.value)}/>
+    <form onSubmit={handleSubmit}>
+      <input
+        className="todo-input"
+        type="text"
+        placeholder="Add Todo"
+        onChange={(e) => setTodo(e.target.value)}
+      />
       <button type="submit" className="add-button">
         Add
       </button>
