7f057a3e8cf403dfedb7cb624684e10f6a6ef434
ozkan <ozkandnz09@gmail.com>
2025-02-02 21:50:58 +0300

    todo

--- src/components/todo/TodoInput.js
+++ src/components/todo/TodoInput.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoInput = () => {
+  const [todo, setTodo] = useState("");
   return (
     <form>
-      <input className="todo-input" type="text" placeholder="Add Todo" />
-      <button type="submit" className="add-button">Add</button>
+      <input className="todo-input" type="text" placeholder="Add Todo" onChange={(e)=>setTodo(e.target.value)}/>
+      <button type="submit" className="add-button">
+        Add
+      </button>
     </form>
   );
 };
