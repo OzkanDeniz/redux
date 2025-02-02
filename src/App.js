174f0db8fe31c76f0a5fefb128b02700e315b140
ozkan <ozkandnz09@gmail.com>
2025-02-02 21:08:17 +0300

    todo

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import Counter from "./components/counter/Counter";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
+import Todo from "./components/todo/Todo"
 
 const App = () => {
   return (
     <div className="app">
       <Provider store={store}>
         <Counter />
+        <Todo/>
+
       </Provider>
     </div>
   );
---
+++ src/components/todo/TodoInput.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TodoInput = () => {
+  return (
+    <form>
+      <input className="todo-input" type="text" placeholder="Add Todo" />
+      <button type="submit" className="add-button">Add</button>
+    </form>
+  );
+};
+
+export default TodoInput;
