646603abc779a6c3653cf8b848fcca35e1076cdb
ozkan <ozkandnz09@gmail.com>
2025-02-02 21:16:07 +0300

    initial

---
+++ src/redux/reducers/todoReducer.jsx
@@ -0,0 +1,12 @@
+const initial = {
+  gorevler: [
+    { id: 0, yazi: "Redux Çalışıyoruz", completed: false },
+    { id: 1, yazi: "köpekleri gezdir", completed: true },
+  ],
+};
+
+const todoReducer = (state = initial, { type, payload }) => {
+  return <div></div>;
+};
+
+export default todoReducer;
